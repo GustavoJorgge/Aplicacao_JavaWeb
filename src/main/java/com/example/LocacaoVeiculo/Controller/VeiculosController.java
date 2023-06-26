a9e65fb44d78c6df4d2012cd1479b47a52024c3e
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2023-06-26 20:03:00 -0300

    Fazendo redirecionamento

--- src/main/java/com/example/LocacaoVeiculo/Controller/VeiculosController.java
+++ src/main/java/com/example/LocacaoVeiculo/Controller/VeiculosController.java
@@ -3,6 +3,7 @@ package com.example.LocacaoVeiculo.Controller;
 import com.example.LocacaoVeiculo.domain.veiculo.DadosCadastroVeiculos;
 import com.example.LocacaoVeiculo.domain.veiculo.Veiculo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,23 +16,25 @@ import java.util.List;
 public class VeiculosController {
 
     private List<Veiculo> veiculos = new ArrayList<>();
-    
+
     @GetMapping("/cadastro") //QUando haver uma requisição /veiculos/formulario ira executar este metodo
     public String apresentaPaginaCadastro(){
         return "veiculos/cadastro";
     }
 
-    @GetMapping
-    public String apresentaPaginaListagem(){
+    @GetMapping("listaveiculos") //QUando haver uma requisição /veiculos/listaveiculos ira executar este medoto
+    public String apresentaPaginaListagem(Model model){
+        model.addAttribute("lista",veiculos);//Enviando a informação para pagina HTML
         return "veiculos/listaveiculos";
     }
 
     @PostMapping
     public String cadastraVeiculo(DadosCadastroVeiculos dados){
         var veiculo = new Veiculo(dados);
-       veiculos.add(veiculo);
-       System.out.println(veiculos);
-        return "veiculos/cadastro";
+        veiculos.add(veiculo);
+        System.out.println(veiculos);
+
+        return "redirect:/veiculos/listaveiculos"; //ao cadastrar um veiculo, ira redirecionar para pagina /listaveiculos
     }
 
 }
